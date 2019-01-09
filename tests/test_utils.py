2ebb2320dca0b50c4e53aa89f53ab0fa5b30dabe
Sondre <sondre.halvorsen@nav.no>
2019-01-09 09:48:06 +0100

    Add test for read_env()

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,3 +4,8 @@ import pytest
 
 def test_read_env__return_object():
     assert utils.read_env() is not None
+
+
+def test_read_env__return_not_empty():
+    res = utils.read_env()
+    assert len(res) > 0
